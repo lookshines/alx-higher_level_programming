a94b2fc9ae7c0c05e1eb03d261d9914b2d1d7b02
lookshines <lookshinesy2k@gmail.com>
2022-12-22 16:20:29 +0000

    code updated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """program that imports functions from the file calculator_1.py"""
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add, sub, mul, div
 
     a = 10
