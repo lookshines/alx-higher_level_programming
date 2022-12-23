c65880e4cda2b765e1796ebbb83297c48a4e4e45
lookshines <lookshinesy2k@gmail.com>
2022-12-23 23:00:18 +0000

    bug fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-    if idx > 0 and idx < (len(my_list) - 1):
+    if idx > 0 and idx <= (len(my_list) - 1):
         my_list[idx] = element
         return my_list
     else:
